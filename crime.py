64e5d6c4210c5266a633271f988413d3cb8e1eb7
AJ <fortinbox123@googlemail.com>
2015-03-29 00:59:23 -0400

    Prevent duplicate group membership, add real crime data

---
+++ crime.py
@@ -0,0 +1,49 @@
+import time
+import numpy
+import random
+import requests
+import json
+class AutoPoster:
+
+
+	def __init__(self, username, groupname, source):
+		self.user=username
+		self.group=groupname
+		self.source=source
+		self.baseUrl = "http://sighting-env.elasticbeanstalk.com/"
+		self.alreadyPosted = []
+
+	def register(self):
+		r = requests.get(self.baseUrl+"register",params={'user':self.user,'pass':"bot"})
+		print r.text
+		r = requests.get(self.baseUrl+"join",params={'user':self.user,'group':self.group,'description':'This group is maintained by a bot','status':5})
+		print r.text
+
+	def checkAlert(self):
+		r = requests.get(self.source)
+		data = r.json()
+		for crime in data["crimes"]:
+			if(not crime["cdid"] in self.alreadyPosted):
+				self.alreadyPosted.append(crime["cdid"])
+				self.postAlert(crime["type"] + ": " + crime["date"],crime["lat"],crime["lon"])
+
+	def postAlert(self,title,lat,lng):
+		r = requests.get(self.baseUrl+"add_alert",params={'user':self.user,'group':self.group,'title':title,'lat':lat,'lng':lng})
+		print r.text
+
+def main():
+	autoposters = []
+
+	crimePoster = AutoPoster("SpotCrime.com","SpotCrime.com","http://api.spotcrime.com/crimes.json?lat=40.3924362&lon=-79.9553335&radius=1&key=MLC-restricted-key")
+	autoposters.append(crimePoster)
+
+	for poster in autoposters:
+		poster.register()
+
+	while(True):
+		for poster in autoposters:
+			poster.checkAlert()
+		time.sleep(60)
+
+if __name__ == "__main__":
+	main()
